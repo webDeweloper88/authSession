689a42e438173e43344a023c8aef68b1bd7624fc
Makhmud <_email@example.com>
2025-07-05 19:30:03 +0500

    "feat: setup Prisma with database schema and Docker Compose for PostgreSQL and Redis"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { IS_DEV_ENV } from './common/utils/is-dev.utils';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { IS_DEV_ENV } from './common/utils/is-dev.utils';
       ignoreEnvFile: !IS_DEV_ENV, // Agar isDev false bo'lsa, .env faylini e'tiborga olmaslik
       envFilePath: IS_DEV_ENV ? '.env' : '.env.production', // Agar isDev true bo'lsa, .env faylini yuklash
     }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
